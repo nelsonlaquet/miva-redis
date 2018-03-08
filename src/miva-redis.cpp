b26f1e7f1bae35030c4e02201ae394eb220707cf
Nelson <nelson@sewingpartsonline.com>
2018-03-08 15:11:00 -0600

    FDS

--- src/miva-redis.cpp
+++ src/miva-redis.cpp
@@ -21,28 +21,34 @@ extern "C" {
 	};
 
 	void redis_connect(mvProgram program, mvVariableHash parameters, mvVariable returnValue, void** pdata)  {
-		// if (_connection != NULL) {
-		// 	const char* error = "";
-		// 	mvProgram_FatalError(program, error, strlen(error));
-		// 	mvVariable_SetValue_Integer(returnValue, 0);
-		// 	return;
-		// }
+		if (_connection != NULL) {
+			const char* error = "";
+			mvProgram_FatalError(program, error, strlen(error));
+			mvVariable_SetValue_Integer(returnValue, 0);
+			return;
+		}
 		
-		// int inputLength = 0;
-		// const char* host = mvVariable_Value(mvVariableHash_Index(parameters, 0), &inputLength);
-		// int port = mvVariable_Value_Integer(mvVariableHash_Index(parameters, 1));
+		int inputLength = 0;
+		const char* host = mvVariable_Value(mvVariableHash_Index(parameters, 0), &inputLength);
+		int port = mvVariable_Value_Integer(mvVariableHash_Index(parameters, 1));
 
-		// _connection = redisConnect(host, port);
-		// if (_connection != NULL && _connection->errstr) {
-		// 	mvProgram_FatalError(program, _connection->errstr, strlen(_connection->errstr));
-		// 	mvVariable_SetValue_Integer(returnValue, 0);
-		// 	return;
-		// }
+		_connection = redisConnect(host, port);
+		if (_connection != NULL && _connection->errstr) {
+			mvProgram_FatalError(program, _connection->errstr, strlen(_connection->errstr));
+			mvVariable_SetValue_Integer(returnValue, 0);
+			return;
+		}
 
 		mvVariable_SetValue_Integer(returnValue, 1);
 	}
 
 	void redis_free(mvProgram program, mvVariableHash parameters, mvVariable returnValue, void** pdata)  {
+		if (_connection != NULL) {
+			redis_free(_connection);		
+			_connection = NULL;
+		}
+
+		mvVariable_SetValue_Integer(returnValue, 1);
 	}
 
 	EXPORT MV_EL_Function_List* miva_function_table() {
